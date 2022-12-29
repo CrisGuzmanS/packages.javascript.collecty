0618880ceb2899ea91553336733dd6436afeb0b2
Cristian Guzman <cristian.guzman.contacto@gmail.com>
2022-12-29 23:11:37 +0000

    NumberCollection decoupled

--- test/collectionTest.test.ts
+++ test/collectionTest.test.ts
@@ -1,21 +1,21 @@
 import Collection from '../Collection'
 
+class NumberCollection extends Collection {
+    item(item) {
+        return new Number(item)
+    }
+}
+
+class Number {
+
+    public item;
+
+    constructor(item) {
+        this.item = item
+    }
+}
+
 test('test one is one', () => {
-
-    class NumberCollection extends Collection {
-        item(item) {
-            return new Number(item)
-        }
-    }
-
-    class Number {
-
-        public item;
-
-        constructor(item) {
-            this.item = item
-        }
-    }
 
     const collection = new NumberCollection([1, 2, 3, 4, 5])
 
