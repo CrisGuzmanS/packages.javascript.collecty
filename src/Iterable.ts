1b8ff948502b5feb2a7b42ed174bb91ae0bffbe8
Cristian Guzman <cristian.guzman.contacto@gmail.com>
2022-12-30 03:49:11 +0000

    removing type module from package.json

--- src/Iterable.ts
+++ src/Iterable.ts
@@ -1,21 +1,38 @@
-export default class Iterable {
+export default class Iterable implements Iterator<any> {
   public items;
+  public index = -1;
 
   constructor(items: any) {
     this.items = items;
   }
 
-  public [Symbol.iterator]() {
-    let index = -1;
-    const data = this.items;
+  public next(): IteratorResult<any> {
+
+    ++this.index;
+    let isDone = false;
+
+    if (!(this.index in this.items)) {
+      isDone = true;
+      this.index = -1
+    }
 
     return {
-      next: () => ({
-        value: this.item(data[++index]),
-        done: !(index in data),
-      }),
-    };
+      done: isDone,
+      value: this.index
+    }
   }
+
+  // public [Symbol.iterator]() {
+  //   let index = -1;
+  //   const data = this.items;
+
+  //   return {
+  //     next: () => ({
+  //       value: this.item(data[++index]),
+  //       done: !(index in data),
+  //     }),
+  //   };
+  // }
 
   item(item: any) {
     return item;
